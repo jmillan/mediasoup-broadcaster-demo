0c8f1c9b9623093a57e2ae4db5c70b498b59e173
hejiayi <janreyho@gmail.com>
2024-06-08 17:24:09 +0800

    upgrade to webrtc m120-6099。 You can successfully enter the room, publish the video stream。 On mac m3max test OK。

--- src/MediaStreamTrackFactory.cpp
+++ src/MediaStreamTrackFactory.cpp
@@ -7,12 +7,26 @@
 #include "pc/test/fake_audio_capture_module.h"
 #include "pc/test/fake_periodic_video_track_source.h"
 #include "pc/test/frame_generator_capturer_video_track_source.h"
+#include "pc/test/fake_video_track_source.h"
 #include "system_wrappers/include/clock.h"
 #include "api/audio_codecs/builtin_audio_decoder_factory.h"
 #include "api/audio_codecs/builtin_audio_encoder_factory.h"
 #include "api/create_peerconnection_factory.h"
 #include "api/video_codecs/builtin_video_decoder_factory.h"
 #include "api/video_codecs/builtin_video_encoder_factory.h"
+
+#include "api/video_codecs/video_decoder_factory.h"
+#include "api/video_codecs/video_decoder_factory_template.h"
+#include "api/video_codecs/video_decoder_factory_template_dav1d_adapter.h"
+#include "api/video_codecs/video_decoder_factory_template_libvpx_vp8_adapter.h"
+#include "api/video_codecs/video_decoder_factory_template_libvpx_vp9_adapter.h"
+#include "api/video_codecs/video_decoder_factory_template_open_h264_adapter.h"
+#include "api/video_codecs/video_encoder_factory.h"
+#include "api/video_codecs/video_encoder_factory_template.h"
+#include "api/video_codecs/video_encoder_factory_template_libaom_av1_adapter.h"
+#include "api/video_codecs/video_encoder_factory_template_libvpx_vp8_adapter.h"
+#include "api/video_codecs/video_encoder_factory_template_libvpx_vp9_adapter.h"
+#include "api/video_codecs/video_encoder_factory_template_open_h264_adapter.h"
 
 using namespace mediasoupclient;
 
@@ -21,21 +35,23 @@ static rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface> factory;
 /* MediaStreamTrack holds reference to the threads of the PeerConnectionFactory.
  * Use plain pointers in order to avoid threads being destructed before tracks.
  */
-static rtc::Thread* networkThread;
-static rtc::Thread* signalingThread;
-static rtc::Thread* workerThread;
+// static rtc::Thread* networkThread;
+// static rtc::Thread* signalingThread;
+  std::unique_ptr<rtc::Thread> signalingThread;
+// static rtc::Thread* workerThread;
 
 static void createFactory()
 {
-	networkThread   = rtc::Thread::Create().release();
-	signalingThread = rtc::Thread::Create().release();
-	workerThread    = rtc::Thread::Create().release();
+	// networkThread   = rtc::Thread::Create().release();
+	signalingThread = rtc::Thread::CreateWithSocketServer();
+	// workerThread    = rtc::Thread::Create().release();
 
-	networkThread->SetName("network_thread", nullptr);
+	// networkThread->SetName("network_thread", nullptr);
 	signalingThread->SetName("signaling_thread", nullptr);
-	workerThread->SetName("worker_thread", nullptr);
+	// workerThread->SetName("worker_thread", nullptr);
 
-	if (!networkThread->Start() || !signalingThread->Start() || !workerThread->Start())
+	// if (!networkThread->Start() || !signalingThread->Start() || !workerThread->Start())
+	if(!signalingThread->Start())
 	{
 		MSC_THROW_INVALID_STATE_ERROR("thread start errored");
 	}
@@ -49,14 +65,24 @@ static void createFactory()
 	}
 
 	factory = webrtc::CreatePeerConnectionFactory(
-	  networkThread,
-	  workerThread,
-	  signalingThread,
+	  nullptr,
+	  nullptr,
+	  signalingThread.get(),
 	  fakeAudioCaptureModule,
 	  webrtc::CreateBuiltinAudioEncoderFactory(),
 	  webrtc::CreateBuiltinAudioDecoderFactory(),
-	  webrtc::CreateBuiltinVideoEncoderFactory(),
-	  webrtc::CreateBuiltinVideoDecoderFactory(),
+	//   webrtc::CreateBuiltinVideoEncoderFactory(),
+	//   webrtc::CreateBuiltinVideoDecoderFactory(),
+      std::make_unique<webrtc::VideoEncoderFactoryTemplate<
+          webrtc::LibvpxVp8EncoderTemplateAdapter,
+          webrtc::LibvpxVp9EncoderTemplateAdapter,
+          webrtc::OpenH264EncoderTemplateAdapter,
+          webrtc::LibaomAv1EncoderTemplateAdapter>>(),
+      std::make_unique<webrtc::VideoDecoderFactoryTemplate<
+          webrtc::LibvpxVp8DecoderTemplateAdapter,
+          webrtc::LibvpxVp9DecoderTemplateAdapter,
+          webrtc::OpenH264DecoderTemplateAdapter,
+          webrtc::Dav1dDecoderTemplateAdapter>>(),
 	  nullptr /*audio_mixer*/,
 	  nullptr /*audio_processing*/);
 
@@ -77,7 +103,7 @@ rtc::scoped_refptr<webrtc::AudioTrackInterface> createAudioTrack(const std::stri
 
 	rtc::scoped_refptr<webrtc::AudioSourceInterface> source = factory->CreateAudioSource(options);
 
-	return factory->CreateAudioTrack(label, source);
+	return factory->CreateAudioTrack(label, source.get());
 }
 
 // Video track creation.
@@ -98,10 +124,11 @@ rtc::scoped_refptr<webrtc::VideoTrackInterface> createSquaresVideoTrack(const st
 		createFactory();
 
 	std::cout << "[INFO] getting frame generator" << std::endl;
-	auto* videoTrackSource = new rtc::RefCountedObject<webrtc::FrameGeneratorCapturerVideoTrackSource>(
+	auto videoTrackSource = rtc::make_ref_counted<webrtc::FrameGeneratorCapturerVideoTrackSource>(
 	  webrtc::FrameGeneratorCapturerVideoTrackSource::Config(), webrtc::Clock::GetRealTimeClock(), false);
-	videoTrackSource->Start();
+	// videoTrackSource->Start();
 
 	std::cout << "[INFO] creating video track" << std::endl;
-	return factory->CreateVideoTrack(rtc::CreateRandomUuid(), videoTrackSource);
+	return factory->CreateVideoTrack(videoTrackSource, rtc::CreateRandomUuid());
+	// return factory->CreateVideoTrack(webrtc::FakeVideoTrackSource::Create(), rtc::CreateRandomUuid());
 }
